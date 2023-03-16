127abfce7c127d163578bbe14bd68e809eff67b1
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-16 14:03:46 +0100

    update the index file

--- src/Components/AllRoutes/AllRoutes.tsx
+++ src/Components/AllRoutes/AllRoutes.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import Login from '../Auth/Login'
+import Register from '../Auth/Register'
+import HomeScreen from '../HomeScreen'
+import LandingPage from "../LandingPage"
 
 const AllRoutes = () => {
-  return (
-    <div>AllRoutes</div>
-  )
+    let element =useRoutes ([
+        {
+            path:"/",
+            element:<HomeScreen/>
+        },
+        {
+            path:"signin",
+            element:<Register/>
+        },
+        {
+            path:"/login",
+            element:<Login/>
+        },
+        {
+            path:"/register",
+            element:<Register/>
+        },{
+            path:"/homepage",
+            element:<LandingPage/>
+        }
+    ])
+  return element;
 }
 
 export default AllRoutes
