8f36d895968e02be6b87aa60dcbfb2f26ebed7ee
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-16 17:30:16 +0100

    updated

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import pic from "./Assets/aboutwater.jpg";
-import guy from "./Assets/guy.jpg";
+// import pic from "./Assets/aboutwater.jpg";
+import pic from "../../Components/Assets/aboutwater.jpg"
+import guy from "../../Components/Assets/guy.jpg";
 
 const About = () => {
   return (
