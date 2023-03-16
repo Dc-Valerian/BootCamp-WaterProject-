ec233363575713410272d24e30b2435424e172cc
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-16 16:19:35 +0100

    added the hero page

--- src/Components/Home/Chart.tsx
+++ src/Components/Home/Chart.tsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import styled from 'styled-components'
+import chart from "../../Components/Assets/chart.png"
 
 const Chart = () => {
   return (
-    <div>Chart</div>
+    <div>
+        <Container>
+            <Pic>
+                <Img src={chart}/>
+            </Pic>
+        </Container>
+    </div>
   )
 }
 
 export default Chart
+const Img = styled.img`
+
+@media screen and (max-width: 500px) {
+  width:100%;
+  height: 100%;
+  object-fit: cover;
+  margin-top: 200px;
+  background-color: gray;
+  margin-bottom: 50px;
+}
+
+@media screen and (max-width: 768px) {
+  width:100%;
+  height: 100%;
+  object-fit: cover;
+  margin-top: 50px;
+  background-color: gray;
+  border-radius: 20px;
+  margin-bottom: 20px;
+  
+}
+`
+
+const Pic = styled.div`
+margin-top: -40px;
+box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+
+@media screen and (max-width: 500px) {
+  margin-top: 0;
+  
+}
+`
+
+
+const Container = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`
--- src/Components/HomeScreen.tsx
+++ src/Components/HomeScreen.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Header from './Header/Header'
+import Chart from './Home/Chart'
+import Deliver from './Home/Deliver'
+import Footer from './Home/Footer'
+import Hero from './Home/Hero'
+import Online from './Home/Online'
+import Story from './Home/Story'
 
 const HomeScreen = () => {
   return (
     <div>
         <Header/>
+        <Hero/>
+        <Online/>
+        <Chart/>
+        <Story/>
+        <Deliver/>
+        <Footer/>
     </div>
   )
 }
