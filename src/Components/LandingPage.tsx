02172b0b71488e35b1b7c336cfe2a5587c0d1185
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-17 01:24:00 +0100

    added new folders

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from './Header/Header'
+import Card from './LandingPage/Card'
+import Hero from './LandingPage/Hero2'
+import Text from './LandingPage/Text'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Header/>
+      <Hero/>
+      <Text/>
+      <Card/>
+    </div>
   )
 }
 
--- src/Components/LandingPage/Text.tsx
+++ src/Components/LandingPage/Text.tsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import styled from 'styled-components'
+import bg from "../../Components/Assets/line.png"
+
 
 const Text = () => {
   return (
-    <div>Text</div>
+    <div>
+        <Container>
+           
+                <Txt>
+                    Natural Mineral Water Comes From <br /> Underground Reserves, Bearing Nature's Own <br />
+                    Original Purity! It Contributes.
+              
+                </Txt>
+           
+        </Container>
+    </div>
   )
 }
 
 export default Text
+
+
+const Txt = styled.div`
+text-align: center;
+line-height: 30px;
+font-size: 25px;
+/* background-color: red; */
+
+`
+
+
+
+const Container = styled.div`
+/* background-color: aliceblue; */
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+margin-top: 40px;
+margin-bottom: 30px;
+
+@media screen and (max-width:500px) {
+    margin-bottom: 10px;
+}
+
+@media screen and (max-width:768px) {
+    /* background-color: blue; */
+    margin-top: 90px;
+}
+
+`
